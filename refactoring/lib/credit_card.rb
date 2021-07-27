eef659600a0cd4582428492f39bf090846f0bb64
fieldstyler <tdfields93@gmail.com>
2021-07-27 00:18:57 -0600

    Refactor credit card class with helper methods that pass the Reek test

--- refactoring/lib/credit_card.rb
+++ refactoring/lib/credit_card.rb
@@ -1,30 +1,51 @@
-# This CreditCard class is intended to give a simple way to store and validate
-# credit card numbers.
-
 class CreditCard
   def initialize(number)
     @number = number
   end
 
   def valid?
-    array = @number.split("")
-    reversed = array.reverse
-    doubled = reversed.map.with_index do |num, index|
+    reversed = split_and_reverse(@number)
+    integer_array = convert_indices_to_integers(reversed)
+    doubled = doubled_array(integer_array)
+    total = sum_array(doubled)
+    check_validity(total)
+  end
+
+  def split_and_reverse(array)
+    array.split("").reverse
+  end
+
+  def doubled_array(array)
+    doubled = array.map.with_index do |num, index|
       if index % 2 == 0
-        num.to_i
+        num
       else
-        num.to_i * 2
+        num * 2
       end
     end
-    summed = doubled.map do |num|
+    doubled
+  end
+
+  def convert_indices_to_integers(array)
+    integer = array.map do |num|
+      num.to_i
+    end
+    integer
+  end
+
+  def sum_array(array)
+    summed = array.map do |num|
       if num > 9
         num - 9
       else
         num
       end
     end
-    total = summed.sum
-    if total % 10 == 0
+    summed.sum
+  end
+
+  def check_validity(integer)
+    if integer % 10 == 0
       true
     else
       false
